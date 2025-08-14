f4beb3a297f79462fa1a97c4cbaa601b17933be0
Sascha Ringert <sascha@ringert.de>
2025-08-14 10:14:26 +0200

    remove dashboard

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,6 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 
